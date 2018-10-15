2b083e9255bcaa3c1875cae9bc021cb662a28c12
Dan Schellenberg <schellenberg@gmail.com>
2018-10-15 15:21:00 -0600

    demos

---
+++ board_demos.py
@@ -0,0 +1,8 @@
+# Interactive Data Type Demos
+# Dan Schellenberg
+# Oct 15, 2018
+
+import random
+
+computers_number = random.randrange(1, 100)
+print(computers_number)
---
+++ data_types_demo_p5.py
@@ -0,0 +1,11 @@
+# My First Program
+# Dan Schellenberg
+# Oct 15, 2018
+
+high_number = input("How high should we count to? ")
+high_number = int(high_number)
+
+counter = 1
+while (counter <= high_number):
+    print(counter)
+    counter = counter + 1
