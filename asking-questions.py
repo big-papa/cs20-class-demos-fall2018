e3266d56c83698b280d116bdd823fba6468fefd0
Dan Schellenberg <schellenberg@gmail.com>
2018-10-16 15:25:17 -0600

    asking questions demo

---
+++ asking-questions.py
@@ -0,0 +1,11 @@
+# Asking a Question Demo
+# Dan Schellenberg
+# Oct 16, 2018
+
+password = input("Please enter the password: ")
+
+if password == "fish":
+    print("The secret message is: The sky is purple.")
+
+else:
+    print("Go away. You can't learn the secret!")
