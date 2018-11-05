43a77687704292f54078d81868c84a62ff729428
Dan Schellenberg <schellenberg@gmail.com>
2018-11-05 09:56:47 -0600

    function intro

---
+++ draw-cross.py
@@ -0,0 +1,25 @@
+import turtle
+
+def draw_cross(some_turtle, side_length):
+    for counter in range(4):
+        for side in range(3):
+            some_turtle.forward(side_length)
+            some_turtle.left(90)
+        some_turtle.right(180)
+
+canvas = turtle.Screen()
+
+mamun = turtle.Turtle()
+mamun.color("blue")
+mamun.pensize(4)
+
+##jenna = turtle.Turtle()
+##jenna.color("purple")
+##jenna.pensize(6)
+
+draw_c(mamun, 300, 125)
+#draw_cross(jenna, 200)
+
+
+
+
---
+++ function-demo.py
@@ -0,0 +1,4 @@
+def say_hello(name):
+    print("Hi there, " + name)
+
+say_hello("Matt")
