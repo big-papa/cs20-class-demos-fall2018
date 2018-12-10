1d25ad8798603a1c6b2f95384a1d53c9625979cf
Dan Schellenberg <schellenberg@gmail.com>
2018-12-10 15:21:19 -0600

    Update vowel_killer.py

--- vowel_killer.py
+++ vowel_killer.py
@@ -7,4 +7,25 @@ def vowel_killer(some_word):
             word_without_vowels = word_without_vowels + letter
     return word_without_vowels
 
-print(vowel_killer("Saskatoon"))
+def even_letters(word):
+    """Returns every other letter from the given word."""
+    new_word = ""
+    for counter in range(len(word)):
+        if counter % 2 == 0:
+            new_word = new_word + word[counter]
+    return new_word
+
+
+def aydens_code(word):
+    """Flips the alphabet for the given word."""
+    flipped_word = ""
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    for letter in word:
+        location = alphabet.find(letter)
+        reversed_location = 25 - location
+        flipped_word = flipped_word + alphabet[reversed_location]
+    return flipped_word
+
+print(aydens_code("ayden"))
+        
+
