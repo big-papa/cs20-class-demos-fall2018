f9f47e028cc29af0c3546a3d1352acc6e74cfb14
Dan Schellenberg <schellenberg@gmail.com>
2018-12-10 08:32:07 -0600

    strings and lists

---
+++ refreshing_lists.py
@@ -0,0 +1,5 @@
+class_names = ["Agnes", "Therrill", "Zach", "Jenna", "Donovan"]
+#print(class_names[4])
+
+some_name = "Zach"
+print(some_name[len(some_name)-1])
---
+++ string-into.py
@@ -0,0 +1,14 @@
+name = "Navaneeth"
+for letter in name:
+    print(letter)
+
+#class_names = ["Robbie", "Aaron", "Issing", "Amal", "Ayden"]
+#for this_name in class_names:
+#    print("Hello " + this_name)
+    
+
+#for counter in range(5):
+#    print("Hello " + class_names[counter])
+
+#my_name = "Yige"
+#print(my_name[len(my_name)])
