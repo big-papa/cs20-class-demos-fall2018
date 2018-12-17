54637bb95594199ec83bbcbaa3577ffe3f96cfc9
Dan Schellenberg <schellenberg@gmail.com>
2018-12-17 14:37:06 -0600

    Update pygame-test.py

--- pygame-test.py
+++ pygame-test.py
@@ -9,12 +9,21 @@ BLUE = (0, 0, 255)
 GREEN = (0, 255, 0)
 
 pygame.init()
-DISPLAYSURF = pygame.display.set_mode((600, 500))
+FPS = 30
+fps_clock = pygame.time.Clock()
+
+width = 1200
+height = 720
+DISPLAYSURF = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Hey CS20!")
 
 pygame.draw.rect(DISPLAYSURF, GREEN, (100, 50, 200, 75))
 
 mario = pygame.image.load("mario.png")
+x = 200
+y = 100
+dx = 5
+dy = 3
 
 #game loop
 while True:
@@ -24,6 +33,11 @@ while True:
             pygame.quit()
             sys.exit()
     
-    x, y = pygame.mouse.get_pos()
+    # move mario
+    x = x + dx
+    y = y + dy
+    
+    #x, y = pygame.mouse.get_pos()
     DISPLAYSURF.blit(mario, (x, y))
     pygame.display.update()
+    fps_clock.tick(FPS)
