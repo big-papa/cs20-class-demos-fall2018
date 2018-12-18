e88051f20c72b7ec60dda048a87886231025adb5
Dan Schellenberg <schellenberg@gmail.com>
2018-12-18 09:42:27 -0600

    Update simon.py

--- simon.py
+++ simon.py
@@ -6,9 +6,31 @@
 import pygame, sys
 from pygame.locals import *
 
+def display_regular_buttons():
+    pygame.draw.rect(DISPLAYSURF, GREEN, green_button)
+    pygame.draw.rect(DISPLAYSURF, RED, red_button)
+    pygame.draw.rect(DISPLAYSURF, YELLOW, yellow_button)
+    pygame.draw.rect(DISPLAYSURF, BLUE, blue_button)
+
+def flash_button(which_button):
+    if which_button == "green":
+        pygame.draw.rect(DISPLAYSURF, GREENFLASH, green_button)
+    elif which_button == "red":
+        pygame.draw.rect(DISPLAYSURF, REDFLASH, red_button)
+    elif which_button == "yellow":
+        pygame.draw.rect(DISPLAYSURF, YELLOWFLASH, yellow_button)
+    elif which_button == "blue":
+        pygame.draw.rect(DISPLAYSURF, BLUEFLASH, blue_button)
+
 def which_button_was_pressed(x, y):
     if green_button.collidepoint( (x, y) ):
         return "green"
+    elif red_button.collidepoint( (x, y) ):
+        return "red"
+    elif yellow_button.collidepoint( (x, y) ):
+        return "yellow"
+    elif blue_button.collidepoint( (x, y) ):
+        return "blue"
 
 pygame.init()
 
@@ -21,22 +43,22 @@ DISPLAYSURF = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Simon Says!")
 
 #         R     G    B         
-GREEN =  (0,   255,  0)
-RED =    (255, 0,    0)
-YELLOW = (255, 255,  0)
-BLUE =   (0,   0,  255)
+GREEN =  (0,   150,  0)
+RED =    (150, 0,    0)
+YELLOW = (150, 150,  0)
+BLUE =   (0,   0,  150)
 BLACK =  (0,   0,    0)
+GREENFLASH = (0, 255, 0)
+REDFLASH =   (255, 0,    0)
+YELLOWFLASH =(255, 255,  0)
+BLUEFLASH =  (0,   0,  255)
 
 green_button = pygame.Rect(100, 100, 300, 300)
 red_button = pygame.Rect(500, 100, 300, 300)
 yellow_button = pygame.Rect(100, 500, 300, 300)
 blue_button = pygame.Rect(500, 500, 300, 300)
 
-#pygame.draw.rect(DISPLAYSURF, BLUE, (300, 100, 200, 50))
-pygame.draw.rect(DISPLAYSURF, GREEN, green_button)
-pygame.draw.rect(DISPLAYSURF, RED, red_button)
-pygame.draw.rect(DISPLAYSURF, YELLOW, yellow_button)
-pygame.draw.rect(DISPLAYSURF, BLUE, blue_button)
+
 
 #background tunes
 #background_music = pygame.mixer.music.load("Mercury.wav")
@@ -47,6 +69,8 @@ pygame.draw.rect(DISPLAYSURF, BLUE, blue_button)
 
 while True:
     #DISPLAYSURF.fill((255, 0, 0))
+    display_regular_buttons()
+    
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
@@ -55,7 +79,8 @@ while True:
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = event.pos
             the_button = which_button_was_pressed(mouse_x, mouse_y)
-            print(the_button)
+            flash_button(the_button)
+            #print(the_button)
             
     #DISPLAYSURF.blit(the_logo, (dvd_x, dvd_y))
     
